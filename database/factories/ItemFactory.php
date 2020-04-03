0f4c70eb96752204f42b3e6a53dd2d5f64140752
PB <b.piotr@gmail.com>
2020-04-03 14:39:07 +0200

    Added migrations for items

--- database/factories/ItemFactory.php
+++ database/factories/ItemFactory.php
@@ -3,10 +3,15 @@
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
 use App\Item;
+use App\Menu;
 use Faker\Generator as Faker;
 
 $factory->define(Item::class, function (Faker $faker) {
+    $menu = factory(Menu::class)->create();
+
     return [
-        //
+        'id' => 1,
+        'name' => $faker->word,
+        'menu_id' => $menu->id
     ];
 });
